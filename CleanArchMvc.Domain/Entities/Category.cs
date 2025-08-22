d61d8acf51ed48af5433708a0c61c3ed8a149c59
Jose Mauro Mendes Junior <josemauromendesjunior@yahoo.com.br>
2025-08-21 22:05:15 -0300

    Aula 31

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,40 @@
+﻿using CleanArchMvc.Domain.Validation;
+using System;
+using System.Collections.Generic;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Category : Entity
+    {
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid id value");
+            this.Id = id;
+            ValidateDomain(name);
+        }
+
+        public string Name { get; private set; }
+
+        public ICollection<Product> Products { get; set; }
+
+        public void Update (string name)
+        {
+            ValidateDomain(name);
+        }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name.Name is required");
+
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimum 3 characters");
+
+            Name = name;
+        }
+    }
+}
