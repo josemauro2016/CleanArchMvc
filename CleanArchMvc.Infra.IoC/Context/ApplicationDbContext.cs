17e232347786e7788126f3d219af9ca843553b45
Jose Mauro Mendes Junior <josemauromendesjunior@yahoo.com.br>
2025-08-24 23:32:19 -0300

    Aula 37

---
+++ CleanArchMvc.Infra.IoC/Context/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMvc.Infra.IoC.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {}
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+            //ApplyConfigurationsFromAssebly procura por todas as classes que implementam IEntityTypeConfiguration e aplica as configurações setadas
+        }
+    }
+}
